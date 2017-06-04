aaf2003af636272fe3f873616e166548eefec001
@genevieveagar Agar <genevieveagar@gmail.com>
2017-06-04 19:41:43 +0000

    Commit Message

---
+++ index.js
@@ -0,0 +1,42 @@
+/*shout*/
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+/*whisper string*/
+function whisper(string){
+  return string.toLowerCase()
+}
+
+/* log Shout */
+function logShout(string){
+  var spy = string
+  spy.toUpperCase
+  spy.andCallThrough
+  console.log('HELLO');
+}
+
+/*log whisper */
+function logWhisper(string){
+  /*var spy = string*/
+  console.log('hello');
+}
+
+function sayHiToGrandma(string){
+  var lowercase
+  lowercase = string
+  if (lowercase.toLowerCase() === lowercase){
+  return "I can't hear you!"
+  }
+
+  var uppercase = string
+  if (uppercase.toUpperCase() === uppercase){
+  return "YES INDEED!"
+  }
+
+  var ILoveYou = string
+  if (ILoveYou = 'I love you, Grandma.') {
+  return "I love you, too."
+  }
+}
